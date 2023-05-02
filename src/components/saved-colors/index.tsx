c035deff9e2aa6ae21a3987982afc1ef20009efe
Gene Hall <genehall@eworldes.com>
2023-05-02 12:11:26 -1000

    update saved colors

--- src/components/saved-colors/index.tsx
+++ src/components/saved-colors/index.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { Dispatch, useState } from 'react';
 import id from 'lodash.uniqueid';
 import AddSavedColor from './add-saved-color';
 import SavedColor from './saved-color';
+import { AdjustColorAction } from '../../color-reducer';
+import { hex } from 'color-convert';
 
 type SavedColorsProps = {
+  dispatch: Dispatch<AdjustColorAction>;
   hexColor: string;
 };
 
@@ -12,11 +15,18 @@ const saved = [
   { id: id(), name: 'Blue Fire', hexColor: '#00aadd' },
 ];
 
-const SavedColors = ({ hexColor }: SavedColorsProps) => {
+const SavedColors = ({ dispatch, hexColor }: SavedColorsProps) => {
   const [savedColors, setSavedColors] = useState(saved);
 
+  const adjustColor = (hex: string) => {
+    dispatch({
+      type: 'update-hex-color',
+      payload: { hexColor: hex },
+    });
+  };
+
   return (
-    <section className="flex flex-col w-full gap-4 sm:col-span-2">
+    <section className="flex w-full flex-col gap-4 sm:col-span-2">
       <h3>Save Color</h3>
       <AddSavedColor
         onSave={(name) =>
@@ -29,6 +39,7 @@ const SavedColors = ({ hexColor }: SavedColorsProps) => {
             key={id}
             name={name}
             hexColor={hexColor}
+            onClick={() => adjustColor(hexColor)}
             onRemove={() =>
               setSavedColors((colors) =>
                 colors.filter((color) => color.id !== id),
